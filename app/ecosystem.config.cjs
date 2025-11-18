514a28349c8f50c8ae6287eb44384ff17395236d
tony-nguyen1 <nguyentoony@gmail.com>
2025-11-18 20:14:26 +0100

    ready to deploy

--- app/ecosystem.config.cjs
+++ app/ecosystem.config.cjs
@@ -1,11 +1,16 @@
 module.exports = {
 	apps: [
 		{
-			script: 'build/server/index.js',
+			script: 'build/index.js',
 			env: {
 				PORT: 3000,
 				HOST: '0.0.0.0',
-				NODE_ENV: 'production'
+				NODE_ENV: 'production',
+
+				ORIGIN: 'http://127.0.0.1:3000',
+
+				DATABASE_URL: process.env.DATABASE_URL,
+				DATABASE_AUTH_TOKEN: process.env.DATABASE_AUTH_TOKEN
 			}
 		}
 	]
