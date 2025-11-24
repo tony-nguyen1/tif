11ee34b021677c6183351f5277d7fee9d7ca4e06
tony-nguyen1 <nguyentoony@gmail.com>
2025-11-24 17:40:29 +0100

    embedded replica

--- app/src/lib/server/db/index.ts
+++ app/src/lib/server/db/index.ts
@@ -6,31 +6,34 @@ import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 if (!dev && !env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
+if (!dev && !env.DATABASE_REPLICA) throw new Error('DATABASE_REPLICA is not set');
+if (!dev && !env.DATABSE_SYNC) throw new Error('DATABSE_SYNC is not set');
 
 console.log(`Node environment=${env.NODE_ENV}`);
 
-const client = createClient({
-	url: env.DATABASE_URL,
-	authToken: env.DATABASE_AUTH_TOKEN
-});
+let tmpClient;
+if (dev) {
+	// Simple file database
+	tmpClient = createClient({
+		url: env.DATABASE_URL!
+	});
+
+	console.info('Using simple file databse');
+} else {
+	// Embedded replica databse
+	tmpClient = createClient({
+		url: env.DATABASE_REPLICA!,
+		authToken: env.DATABASE_AUTH_TOKEN,
+		syncUrl: env.DATABASE_URL,
+		syncInterval: Number(env.DATABSE_SYNC!),
+		offline: true
+	});
+
+	console.info('Using Turso embedded replica database setup');
+	console.info('... syncing');
+	await tmpClient.sync();
+	console.info('... syncing OK');
+}
+const client = tmpClient;
 
 export const db = drizzle(client, { schema });
-
-// if (!dev && !env.DATABASE_REPLICA) throw new Error('DATABASE_REPLICA is not set');
-// if (!dev && !env.DATABSE_SYNC) throw new Error('DATABSE_SYNC is not set');
-// let tmpClient;
-// if (dev) {
-// 	// Simple file database
-// 	tmpClient = createClient({
-// 		url: env.DATABASE_URL!
-// 	});
-// } else {
-// 	// Embedded replica databse
-// 	tmpClient = createClient({
-// 		url: env.DATABASE_REPLICA!,
-// 		authToken: env.DATABASE_AUTH_TOKEN,
-// 		syncUrl: env.DATABASE_URL,
-// 		syncInterval: Number(env.DATABSE_SYNC!)
-// 	});
-// }
-// const client = tmpClient;
