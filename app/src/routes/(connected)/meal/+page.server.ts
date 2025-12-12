ce108ead0d3eacc2ae6585566d22671a59e5ac66
tony-nguyen1 <nguyentoony@gmail.com>
2025-12-12 16:28:10 +0100

    better form validation

--- app/src/routes/(connected)/meal/+page.server.ts
+++ app/src/routes/(connected)/meal/+page.server.ts
@@ -1,5 +1,5 @@
 import type { Actions, PageServerLoad } from './$types.js';
-import { createMeal, deleteMeal, findLatestMealOf } from '$lib/server/db/mealRepo.js';
+import { createMeal, deleteMeal, findLatestMealOf, findMeal } from '$lib/server/db/mealRepo.js';
 import * as table from '$lib/server/db/schema';
 import { _requireLogin } from '../workout/+page.server';
 import { findMealOfUser, editMeal } from '$lib/server/db/mealRepo.js';
@@ -59,7 +59,6 @@ export const actions: Actions = {
 
 		const userId = _requireLogin().id;
 
-		// FIXME : remove userId inside form
 		const input = {
 			userId: userId,
 			date: new Date(),
@@ -90,14 +89,65 @@ export const actions: Actions = {
 		const data = await request.formData();
 		const userId = _requireLogin().id;
 
+		await sleep(2000);
+
+		if (!data.get('description')) {
+			return fail(400, { missing: true, message: 'Form is missing description input' });
+		}
+
+		if (!data.get('fullness')) {
+			return fail(400, { missing: true, message: 'Form is missing fullness input' });
+		}
+
+		if (!data.get('protein')) {
+			return fail(400, { missing: true, message: 'Form is missing protein input' });
+		}
+
+		if (!data.get('place')) {
+			return fail(400, { missing: true, message: 'Form is missing place input' });
+		}
+
+		if (!data.get('place')) {
+			return fail(400, { missing: true, message: 'Form is missing place input' });
+		}
+
+		console.info(data);
+		const tmpFullness = Number(data.get('fullness')!.toString());
+		if (tmpFullness > 0 && tmpFullness <= 10) {
+			return fail(400, {
+				incorrect: true,
+				message: 'Fulness input must be between 1 and 10 included'
+			});
+		}
+
+		const tmpProtein = Number(data.get('fullness')!.toString());
+		if (tmpProtein >= 0) {
+			return fail(400, {
+				incorrect: true,
+				message: 'Protein input must be positive'
+			});
+		}
+
+		const mealId = Number(data.get('mealId')!.toString());
+		const mealFromDb = await findMeal(mealId);
+		if (!mealFromDb) {
+			return fail(404, { message: `Meal id ${mealId} not found` });
+		}
+
+		if (mealFromDb?.userId !== userId) {
+			return fail(403, {
+				message: "User isn't authorized to update this meal"
+			});
+		}
+
 		const input = {
 			// date: new Date(data.get('date')!.toString()),
-			id: Number(data.get('mealId')!.toString()),
+			id: mealId,
 			userId: userId,
 			description: data.get('description')!.toString(),
 			place: data.get('place')!.toString(),
-			protein: Number(data.get('protein')!.toString()),
-			fullness: Number(data.get('fullness')!.toString())
+			protein: tmpProtein,
+			fullness: tmpFullness
 		};
 
 		await editMeal(input);
