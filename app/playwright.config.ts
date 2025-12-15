753a034b0771d5c60f14c0d6a837b721c582d17d
tony-nguyen1 <nguyentoony@gmail.com>
2025-12-15 16:23:10 +0100

    cleaning up CI workflow 14

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -4,14 +4,17 @@ export default defineConfig({
 	use: {
 		headless: true,
 		viewport: { width: 1280, height: 720 },
-		actionTimeout: 30000,
+		actionTimeout: 2000,
 		baseURL: 'http://localhost:4173' // process.env.ORIGIN // Vite dev server URL
 	},
 	webServer: {
 		command: 'npm run preview:test',
-		reuseExistingServer: false,
-		port: 4173,
-		timeout: 2000
+		reuseExistingServer: true,
+		timeout: 2000,
+		url: 'http://localhost:4173'
+		// wait: {
+		// 	// stdout: '/Listening on port (?<my_server_port>\\d+)/'
+		// }
 	},
 	testDir: 'e2e'
 });
