ad208cff927f4ca3c40126b0a5121de04ea677bf
Tony Nguyen <nguyentoony@gmail.com>
2025-12-15 17:47:34 +0100

    CI: added db migration in pipeline  (#69)
    * added migration of db in ci pipeline
    
    * fixing CI workflow
    
    * fixing CI workflow 2
    
    * fixing CI workflow 3
    
    * fixing CI workflow 4
    
    * fixing CI workflow 5
    
    * cleaning up CI workflow 5
    
    * cleaning up CI workflow 6
    
    * cleaning up CI workflow 7
    
    * cleaning up CI workflow 8
    
    * cleaning up CI workflow 9
    
    * cleaning up CI workflow 10
    
    * cleaning up CI workflow 11
    
    * cleaning up CI workflow 12
    
    * cleaning up CI workflow 13
    
    * cleaning up CI workflow 14
    
    * cleaning up CI workflow 15
    
    * cleaning up CI workflow 16
    
    * cleaning up CI workflow 17
    
    * cleaning up CI workflow 18
    
    * cleaning up CI workflow 19
    
    * cleaning up CI workflow 20
    
    * cleaning up CI workflow 21
    
    * ez workflow to cache playwright
    
    * fixing CI

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -4,13 +4,13 @@ export default defineConfig({
 	use: {
 		headless: true,
 		viewport: { width: 1280, height: 720 },
-		actionTimeout: 30000,
+		actionTimeout: 2000,
 		baseURL: 'http://localhost:4173' // process.env.ORIGIN // Vite dev server URL
 	},
 	webServer: {
 		command: 'npm run preview:test',
-		reuseExistingServer: true,
-		port: 4173
+		reuseExistingServer: false,
+		url: 'http://localhost:4173'
 	},
 	testDir: 'e2e'
 });
