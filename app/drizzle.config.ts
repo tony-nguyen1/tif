61e0f9b63fbf853de6d3514d94476065857adea0
tony-nguyen1 <nguyentoony@gmail.com>
2025-12-15 14:15:15 +0100

    added migration of db in ci pipeline

--- app/drizzle.config.ts
+++ app/drizzle.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 console.info('drizzle.config.ts');
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+console.info(`[drizzle.config.ts] ${process.env.DATABASE_URL}`);
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
