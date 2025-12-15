ff1cd3948860e4fe416123bfeef85a6c1fd99bd0
tony-nguyen1 <nguyentoony@gmail.com>
2025-12-15 16:46:13 +0100

    cleaning up CI workflow 18

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 	webServer: {
 		command: 'npm run preview:test',
 		reuseExistingServer: true,
-		timeout: 30000,
+		// timeout: 60000,
 		url: 'http://localhost:4173'
 		// wait: {
 		// 	// stdout: '/Listening on port (?<my_server_port>\\d+)/'
