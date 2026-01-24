304b43c0adc6a37e50667828467e70f8763c3f93
tony-nguyen1 <nguyentoony@gmail.com>
2026-01-24 15:21:55 +0100

    fix: type issue & warning

--- app/src/lib/components/ui/calendar/calendar.svelte
+++ app/src/lib/components/ui/calendar/calendar.svelte
@@ -52,7 +52,7 @@ get along, so we shut typescript up by casting `value` to `never`.
 	{weekdayFormat}
 	{disableDaysOutsideMonth}
 	class={cn(
-		'group/calendar bg-background p-3 [--cell-size:--spacing(8)] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent',
+		'group/calendar bg-background p-3 [--cell-size:--spacing(8)] in-data-[slot=card-content]:bg-transparent in-data-[slot=popover-content]:bg-transparent',
 		className
 	)}
 	{locale}
