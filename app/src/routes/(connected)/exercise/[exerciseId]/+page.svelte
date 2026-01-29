84c2fe9c5a237262d854371d4f5aef6a4411588a
Tony Nguyen <nguyentoony@gmail.com>
2026-01-29 17:46:12 +0100

    fix: better ui & ux for chart in exercise (#109)
    * saving work
    
    * fix: better ui & ux for chart in exercise

--- app/src/routes/(connected)/exercise/[exerciseId]/+page.svelte
+++ app/src/routes/(connected)/exercise/[exerciseId]/+page.svelte
@@ -1,37 +1,166 @@
 <script lang="ts">
 	import type { PageServerData } from '../../exercise/[exerciseId]/$types';
-	import Chart from '$lib/components/CustomChart.svelte';
+	// import Chart from '$lib/components/CustomChart.svelte';
 	import { resolve } from '$app/paths';
-	import type { ChartData } from 'chart.js';
 	import FormAddSet from '$lib/components/custom/exercise/FormAddSet.svelte';
+	import { onMount } from 'svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as ButtonGroup from '$lib/components/ui/button-group/index.js';
+	import {
+		BarController,
+		BarElement,
+		CategoryScale,
+		Chart,
+		Legend,
+		LinearScale,
+		LineController,
+		LineElement,
+		PointElement,
+		TimeScale,
+		Title,
+		Tooltip
+	} from 'chart.js';
+
+	import 'chartjs-adapter-date-fns';
+	Chart.register(
+		LineElement,
+		PointElement,
+		LineController,
+		BarController,
+		BarElement,
+		TimeScale,
+		CategoryScale,
+		LinearScale,
+		Title,
+		Tooltip,
+		Legend
+	);
+	import { FormState } from './myEnum';
 
 	let { data }: { data: PageServerData } = $props();
-
-	const beepBoop = $derived<() => ChartData<'bar'>>(() => ({
-		labels: data.x,
-		datasets: [
-			{
-				label: 'Volume',
-				data: data.y,
-				backgroundColor: 'rgba(75, 192, 192, 0.4)',
-				borderColor: 'rgba(75, 192, 192, 1)',
-				borderWidth: 1
-			}
-		]
-	}));
-
-	const options = {
-		responsive: true,
-		scales: {
-			y: { beginAtZero: true }
-		}
+	type TimePoint = {
+		x: Date; // or Date
+		y: number;
 	};
+	// const beepBoop = $derived<() => ChartData<'line', TimePoint[]>>(() => ({
+	// 	// labels: data.x,
+	// 	datasets: [
+	// 		{
+	// 			label: 'Volume',
+	// 			data: data.workoutSummary,
+	// 			backgroundColor: 'rgba(255, 255, 255, 1)',
+	// 			borderColor: '#36A2EB',
+	// 			borderWidth: 1.3,
+	// 			pointStyle: 'triangle',
+	// 			pointRadius: 8,
+	// 			tension: 0.3
+	// 		}
+	// 	]
+	// }));
+
+	let interactivitySectionState: FormState = $derived(
+		data.workoutAlreadyExisting ? FormState['AddSet'] : FormState['DisplayGraph']
+	);
+
+	let input = $derived(data.workoutSummary);
+	let canvas: HTMLCanvasElement | null = $state(null);
+	let myChart: Chart<'line', TimePoint[]> | null = null;
+	onMount(() => {
+		myChart = createMyChart(input);
+	});
+
+	$effect(() => {
+		if (myChart) {
+			myChart.destroy();
+			myChart = createMyChart(input);
+			myChart.render();
+		}
+	});
+
+	function createMyChart(input: TimePoint[]) {
+		return new Chart(canvas!, {
+			type: 'line',
+			data: {
+				datasets: [
+					{
+						label: 'Volume',
+						data: input,
+						backgroundColor: 'rgba(255, 255, 255, 1)',
+						borderColor: '#36A2EB',
+						borderWidth: 1.3,
+						pointStyle: 'triangle',
+						pointRadius: 8,
+						tension: 0.3
+					}
+				]
+			},
+			options: {
+				responsive: true,
+				maintainAspectRatio: false,
+				scales: {
+					x: {
+						type: 'time',
+						time: {
+							unit: 'day'
+						},
+						grid: { color: 'rgba(255,255,255, 0.1)' },
+						ticks: {
+							color: 'rgba(255,255,255, 0.6)',
+							font: {
+								size: 16
+							}
+						}
+					},
+					y: {
+						beginAtZero: true,
+						grid: { color: 'rgba(255,255,255, 0.1)' },
+						ticks: {
+							color: '#36A2EB',
+							font: {
+								size: 16
+							}
+						}
+					}
+				},
+				plugins: {
+					legend: { labels: { usePointStyle: true } }
+				}
+			}
+		});
+	}
 </script>
 
 <section class="w-full">
 	<h1 class="text-5xl">{data.exerciseInfo.name}</h1>
 	{#if data.workoutAlreadyExisting}
-		<FormAddSet workout={data.workoutAlreadyExisting}></FormAddSet>
+		<ButtonGroup.Root id="buttonGroupWorkoutForm" class="my-2">
+			<Button
+				variant="outline"
+				class={['size-fit', 'px-2', 'py-1']}
+				onclick={() => (interactivitySectionState = FormState['Hide'])}>Hide</Button
+			>
+			<Button
+				hidden={data.workoutAlreadyExisting ? false : true}
+				variant="outline"
+				class={['size-fit', 'px-2', 'py-1']}
+				onclick={() => (interactivitySectionState = FormState['AddSet'])}>Form</Button
+			>
+			<Button
+				variant="outline"
+				class={['size-fit', 'px-2', 'py-1']}
+				onclick={() => (interactivitySectionState = FormState['DisplayGraph'])}>Graph</Button
+			>
+		</ButtonGroup.Root>
+		<FormAddSet
+			workout={data.workoutAlreadyExisting}
+			hidden={interactivitySectionState !== FormState.AddSet}
+		></FormAddSet>
+		<section
+			class="relative flex h-[35dvh] w-full justify-center"
+			hidden={interactivitySectionState !== FormState.DisplayGraph}
+		>
+			<canvas bind:this={canvas} class="max-w-full"></canvas>
+		</section>
 	{/if}
 	{#if data.workoutList.length === 0}
 		No data
@@ -74,13 +203,5 @@
 				{/if}
 			{/each}
 		</div>
-
-		<Chart
-			data={beepBoop.apply((x: ChartData) => {
-				return x;
-			})}
-			{options}
-			type="line"
-		/>
 	{/if}
 </section>
