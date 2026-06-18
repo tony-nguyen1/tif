29d5c355a60942b4af5d00a2c9cf4dbaeda6778a
tony-nguyen1 <nguyentoony@gmail.com>
2026-06-18 18:10:48 +0200

    feat: about page

--- app/src/routes/(anonym)/+layout.svelte
+++ app/src/routes/(anonym)/+layout.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import forgejoLogo from '$lib/assets/img/forgejo.svg';
+	import githubLogoDark from '$lib/assets/img/github-dark.svg';
+	import githubLogoLight from '$lib/assets/img/github.svg';
+	import linkedInLogo from '$lib/assets/img/linkedIn.webp';
+	import SolarHome2Outline from '@iconify-svelte/solar/home-2-outline';
+	import { SquareArrowOutUpRight } from '@lucide/svelte/icons';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import { ModeWatcher } from 'mode-watcher';
 	import '../../app.css';
 	import type { LayoutProps } from './$types';
-	import favicon from '$lib/assets/favicon.svg';
-	import { ModeWatcher } from 'mode-watcher';
-	import SunIcon from '@lucide/svelte/icons/sun';
-	import MoonIcon from '@lucide/svelte/icons/moon';
-	import SolarHome2Outline from '@iconify-svelte/solar/home-2-outline';
 
-	import { toggleMode } from 'mode-watcher';
-	import { Button } from '$lib/components/ui/button/index.js';
 	import { resolve } from '$app/paths';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { toggleMode } from 'mode-watcher';
 
 	let { children }: LayoutProps = $props();
 </script>
@@ -19,10 +24,38 @@
 </svelte:head>
 
 <ModeWatcher />
-<!-- test -->
 <div class="flex w-full flex-col">
 	<main class="grid grow overflow-y-auto">
 		{@render children?.()}
+		<hr class="mx-6 mt-8 mb-3" />
+		<footer class="grid grid-rows-[auto_auto_auto] justify-items-center pb-2">
+			<div class="flex flex-row gap-x-6">
+				<Button variant="link" href="/about" class="w-fit text-lg">About</Button>
+				<Button variant="link" href="https://github.com/tony-nguyen1/tif" class="w-fit text-lg"
+					>Source<SquareArrowOutUpRight /></Button
+				>
+			</div>
+			<div class="mt-3.5 flex flex-row gap-x-10">
+				<a href="https://github.com/tony-nguyen1">
+					<img src={githubLogoLight} class="block h-7 dark:hidden" alt="Logo Github" />
+					<img src={githubLogoDark} class="hidden h-7 dark:block" alt="Logo Github" />
+				</a>
+				<a href="https://forgejo.nguyentony.fr">
+					<img
+						src={forgejoLogo}
+						alt="Logo Forgejo"
+						class="h-7"
+						title="Self-hosted Forgejo instance"
+					/>
+				</a>
+				<a href="https://www.linkedin.com/in/nguyentoony/">
+					<img src={linkedInLogo} alt="Logo LinkedIn" class="h-7" />
+				</a>
+			</div>
+			<div class="mt-6 text-center text-zinc-600 dark:text-zinc-300">
+				© 2026 Tony Nguyen. MIT License.
+			</div>
+		</footer>
 	</main>
 	<nav class="flex h-fit flex-row place-content-evenly border-t p-3">
 		<Button variant="link" href={resolve('/')} aria-label="Home"
--- app/src/routes/(anonym)/+page.svelte
+++ app/src/routes/(anonym)/+page.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
+	import loginPage from '$lib/assets/img/login_page.webp';
 	import mealPage from '$lib/assets/img/meal_page.webp';
 	import sleepPage from '$lib/assets/img/sleep_page.webp';
 	import weightPage from '$lib/assets/img/weight_page.webp';
-	import forgejoLogo from '$lib/assets/img/forgejo.svg';
-	import githubLogoLight from '$lib/assets/img/github.svg';
-	import githubLogoDark from '$lib/assets/img/github-dark.svg';
-	import linkedInLogo from '$lib/assets/img/linkedIn.webp';
-	import loginPage from '$lib/assets/img/login_page.webp';
-	import {
-		Zap,
-		MousePointer2,
-		Milestone,
-		BadgeEuro,
-		Sparkles,
-		SquareArrowOutUpRight
-	} from '@lucide/svelte/icons';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { BadgeEuro, Milestone, MousePointer2, Sparkles, Zap } from '@lucide/svelte/icons';
 </script>
 
 <section class="mt-12 mb-8 grid gap-y-6 px-4">
@@ -98,27 +87,3 @@
 		/>
 	</article>
 </section>
-<hr class="mx-6 mt-8 mb-3" />
-<footer class="grid grid-rows-[auto_auto_auto] justify-items-center pb-2">
-	<div class="flex flex-row gap-x-6">
-		<Button variant="link" href="/about" class="w-fit text-lg">About</Button>
-		<Button variant="link" href="https://github.com/tony-nguyen1/tif" class="w-fit text-lg"
-			>Source<SquareArrowOutUpRight /></Button
-		>
-	</div>
-	<div class="mt-3.5 flex flex-row gap-x-10">
-		<a href="https://github.com/tony-nguyen1">
-			<img src={githubLogoLight} class="block h-7 dark:hidden" alt="Logo Github" />
-			<img src={githubLogoDark} class="hidden h-7 dark:block" alt="Logo Github" />
-		</a>
-		<a href="https://forgejo.nguyentony.fr">
-			<img src={forgejoLogo} alt="Logo Forgejo" class="h-7" title="Self-hosted Forgejo instance" />
-		</a>
-		<a href="https://www.linkedin.com/in/nguyentoony/">
-			<img src={linkedInLogo} alt="Logo LinkedIn" class="h-7" />
-		</a>
-	</div>
-	<div class="mt-6 text-center text-zinc-600 dark:text-zinc-300">
-		© 2026 Tony Nguyen. MIT License.
-	</div>
-</footer>
