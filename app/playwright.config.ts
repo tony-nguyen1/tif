a9704c6992a8f7db72c0120558eb248906f67653
tony-nguyen1 <nguyentoony@gmail.com>
2026-06-28 19:10:05 +0200

    e2e tests in docker container in ci

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -1,20 +1,23 @@
 import { defineConfig } from '@playwright/test';
 
+const port = process.env.HOST_PORT!;
+const origin = process.env.ORIGIN ?? `http://localhost:${port}`;
+
 export default defineConfig({
 	use: {
 		headless: true,
 		viewport: { width: 1280, height: 720 },
 		actionTimeout: 2000,
-		baseURL: 'http://localhost:4173',
+		baseURL: origin,
 		launchOptions: {
 			// slowMo: 3000
 		}
 	},
-	webServer: {
-		command: 'npm run preview:test',
-		reuseExistingServer: false,
-		timeout: 120 * 1000,
-		url: 'http://localhost:4173'
-	},
+	// webServer: {
+	// 	command: 'npm run preview:test',
+	// 	reuseExistingServer: true,
+	// 	timeout: 120 * 1000,
+	// 	url: 'http://localhost:3004'
+	// },
 	testDir: 'e2e'
 });
