fe140d85f46aad63b230a20814eec66c6a692775
Tony Nguyen <nguyentoony@gmail.com>
2026-07-01 20:50:44 +0200

    fix(devops): adding back the deps (#141)
    * fix(devops): adding back the deps
    
    Copying node_modules into the final image

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -4,6 +4,7 @@ const port = process.env.HOST_PORT!;
 const origin = process.env.ORIGIN ?? `http://localhost:${port}`;
 
 export default defineConfig({
+	reporter: process.env.CI ? 'github' : 'list',
 	use: {
 		headless: true,
 		viewport: { width: 1280, height: 720 },
