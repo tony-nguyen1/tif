5d4d78590cb13c03fb9c6dc023c0a2321b8e6f13
Tony Nguyen <nguyentoony@gmail.com>
2026-07-01 15:46:44 +0200

    fix(devops): docker and better pipeline (#139)
    * fix(pipeline): added a check, building up to dockerize my app
    
    * sanity check
    
    * fix: path fix
    
    * test
    
    * setting up Dockerfile
    
    * updating actions/checkout version
    
    * fix ?
    
    * bumping actions version
    
    * bumping action cache version
    
    * e2e tests in docker container in ci
    
    * better runtime check, removing it from build time
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * tweaking ci workflow
    
    * fix
    
    * optimisation
    
    * fix & added a few jobs and steps
    
    * fix
    
    * fix
    
    * removed scan job
    
    * fix version
    
    * fix
    
    * some changes
    
    * opti
    
    * better config
    
    * fix
    
    * fix
    
    * trying something
    
    * fix: cicd better with docker

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -1,7 +1,8 @@
-import type { Handle } from '@sveltejs/kit';
-import * as auth from '$lib/server/auth';
 import { env } from '$env/dynamic/private';
-// import { client } from '$lib/server/db';
+import * as auth from '$lib/server/auth';
+import { db, initDb, isTest } from '$lib/server/db';
+import type { Handle } from '@sveltejs/kit';
+import { migrate } from 'drizzle-orm/libsql/migrator';
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
@@ -27,13 +28,18 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 
 export const handle: Handle = handleAuth;
 
-// console.info('[hooks.server.ts] running once');
-export const isProd = env.APP_ENV === 'production';
-if (isProd) {
-	// console.info('[hooks.server.ts] syncing db');
-	// console.info('... syncing');
-	// const res = await client.sync();
-	// console.info(res);
-	// console.info(`... frames applied=${res?.frames_synced ?? 'N/A'}`);
-	// console.info('... syncing OK');
+// checking for necesary env variables
+export async function init() {
+	if (!env.APP_ENV) throw new Error('APP_ENV is not set');
+	if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+	if (!['development', 'test', 'production'].includes(env.APP_ENV)) {
+		throw new Error('APP_ENV must be one of: development, test, production');
+	}
+
+	initDb();
+
+	if (isTest) {
+		console.info('... running migration');
+		await migrate(db, { migrationsFolder: 'drizzle' });
+	}
 }
--- app/src/lib/server/db/index.ts
+++ app/src/lib/server/db/index.ts
@@ -1,84 +1,50 @@
 import { dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import { createClient } from '@libsql/client';
-import { drizzle } from 'drizzle-orm/libsql';
-import { migrate } from 'drizzle-orm/libsql/migrator';
+import { drizzle, LibSQLDatabase } from 'drizzle-orm/libsql';
 import * as schema from './schema';
 import fs from 'fs';
 
 console.info('index.ts');
 
-if (!env.APP_ENV) throw new Error('APP_ENV is not set');
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-console.log(`Node environment=${env.NODE_ENV}`);
-console.log(`App environment=${env.APP_ENV}`);
-console.log(`Database url=${env.DATABASE_URL}`);
-
 export const isProd = env.APP_ENV === 'production';
 export const isTest = env.APP_ENV === 'test';
 export const isDev = env.APP_ENV === 'development';
 
-let tmpClient;
-if (dev) {
-	console.info('Using simple file database');
-	tmpClient = createClient({
-		url: env.DATABASE_URL
-	});
-} else {
-	// running preview, built/optimized version
-	if (isDev) {
-		// for debugging purposes
-		console.info(`Using Turso file database`);
-		tmpClient = createClient({
-			url: env.DATABASE_URL
-		});
+export let client: ReturnType<typeof createClient>;
+export let db: LibSQLDatabase<typeof schema>;
+
+export function initDb() {
+	console.info('initDb running');
+	console.log(`Node environment=${env.NODE_ENV}`);
+	console.log(`App environment=${env.APP_ENV}`);
+	console.log(`Database url=${env.DATABASE_URL}`);
+
+	let tmpClient;
+
+	if (dev) {
+		console.info('Using simple file database');
+		tmpClient = createClient({ url: env.DATABASE_URL! });
 	} else if (isTest) {
-		// for e2e tests
 		const filePath = env.DATABASE_URL!.replace('file:', '');
 		if (fs.existsSync(filePath)) {
-			console.info('... test db already exist');
+			console.info('... test db already exists');
 			fs.unlinkSync(filePath);
 			console.info('... test db deleted');
 		}
-
 		console.info(`Using Turso file database ${env.DATABASE_URL}`);
-		tmpClient = createClient({
-			url: env.DATABASE_URL
-		});
+		tmpClient = createClient({ url: env.DATABASE_URL! });
 	} else if (isProd) {
-		// if (!env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
-		// if (!env.DATABASE_REPLICA) throw new Error('DATABASE_REPLICA is not set');
-		// if (!env.DATABASE_SYNC) throw new Error('DATABASE_SYNC is not set');
-
-		// console.info(
-		// 	`Using Turso cloud/Embedded replica database setup\nsyncinterval set at ${Number(env.DATABASE_SYNC)} sec`
-		// );
-		// // Embedded replica database
-		// tmpClient = createClient({
-		// 	url: env.DATABASE_REPLICA,
-		// 	authToken: env.DATABASE_AUTH_TOKEN,
-		// 	syncUrl: env.DATABASE_URL,
-		// 	syncInterval: Number(env.DATABASE_SYNC)
-		// });
-		console.info(`Using simple Turso cloud database`);
+		console.info('Using Turso cloud database');
 		tmpClient = createClient({
-			url: env.DATABASE_URL,
-			authToken: env.DATABASE_AUTH_TOKEN
+			url: env.DATABASE_URL!,
+			authToken: env.DATABASE_AUTH_TOKEN!
 		});
 	} else {
-		throw new Error(
-			`APP_ENV environment variable not set properly. Choose either: development, test or production`
-		);
+		console.info('Using Turso file database (default)');
+		tmpClient = createClient({ url: env.DATABASE_URL! });
 	}
-}
 
-export const client = tmpClient;
-export const db = drizzle(tmpClient, { schema });
-
-if (isTest) {
-	console.info('... running migration');
-	await migrate(db, {
-		migrationsFolder: 'drizzle'
-	});
+	client = tmpClient;
+	db = drizzle(tmpClient, { schema });
 }
